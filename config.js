dfb204f0720df9c9df596a0ab1d9798dc59501c9
eyjolfurben <eyjolfurben@gmail.com>
2025-10-19 23:09:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js
-window.CONFIG = {
+const CONFIG = {
   APPROVAL_ENDPOINT: "https://script.google.com/macros/s/AKfycbypvzehel0Fs6D7HI19V8V2osIA1lRBibns9Efc-eWMlgDSPuHryGwPgmknmrlPD9aztg/exec"
 };
