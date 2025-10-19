ed79d57f84d42f4aafc082b28bd98957d6879651
eyjolfurben <eyjolfurben@gmail.com>
2025-10-19 23:23:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-const CONFIG = {
-  APPROVAL_ENDPOINT: "https://script.google.com/macros/s/AKfycbypvzehel0Fs6D7HI19V8V2osIA1lRBibns9Efc-eWMlgDSPuHryGwPgmknmrlPD9aztg/exec"
+// config.js
+// Sameinaður endpoint fyrir "request" + "approve" (Google Apps Script).
+window.CONFIG = {
+  APP_ENDPOINT: "https://script.google.com/macros/s/AKfycbypvzehel0Fs6D7HI19V8V2osIA1lRBibns9Efc-eWMlgDSPuHryGwPgmknmrlPD9aztg/exec"
 };
