07afaeeb696181622519aac9795b1bc368d7c853
eyjolfurben <eyjolfurben@gmail.com>
2025-10-19 23:53:39 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-// Sameinaður endpoint fyrir "request" + "approve" (Google Apps Script).
+// Sameinaður endpoint fyrir "request" (bókunarbeiðni) og "approve" (samþykki).
 window.CONFIG = {
   APP_ENDPOINT: "https://script.google.com/macros/s/AKfycbypvzehel0Fs6D7HI19V8V2osIA1lRBibns9Efc-eWMlgDSPuHryGwPgmknmrlPD9aztg/exec"
 };
